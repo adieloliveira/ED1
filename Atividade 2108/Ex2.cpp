970196f23edfc063e8791d4134e68770b355e29a
adieloliveira <72995521+adieloliveira@users.noreply.github.com>
2020-10-16 15:54:51 -0300

    Add files via upload

---
+++ Atividade 2108/Ex2.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+using namespace std;
+
+void comparaMatrizes(int *x[9], int *z[9])
+{
+	bool iguais;
+	int verificador = 0;
+	
+	for(int i=0; i<3; ++i)
+	{
+		for(int j=0; j<3; ++j)
+	    {
+			if(x[i][j] == z[i][j] && verificador == 0)
+			{
+				iguais = true;
+			}
+			else {
+				iguais = false;
+				verificador = 1;
+			}	
+	    }
+	}
+	
+	string resposta = (iguais) ? "As matrizes sao iguais!" : "As matrizes sao diferentes";
+	
+	cout << resposta << endl;
+}
+
+int main(int argc, char** argv)
+{
+	int **mat1;
+	int **mat2;
+	int contador = 0;
+		
+	cout << "Matriz 1" << endl;
+	
+	mat1 = new int*[3];
+	for(int i=0; i<3; ++i)
+	{
+		
+		mat1[i] = new int[3];
+		for(int j=0; j<3; ++j)
+	    {
+	    	contador++;
+	    	
+	    	cout << "Digite o valor " << contador << ":";
+			cin >> mat1[i][j];
+	    }
+	}
+	for(int i=0; i<3; ++i)
+	{
+	    for(int j=0; j<3; ++j)
+	    {
+            cout << mat1[i][j] << " | ";
+		}
+		cout << endl; 
+	}	
+		
+	contador = 0;
+	
+	cout << "Matriz 2" << endl;
+	
+	mat2 = new int*[3];
+	for(int i=0; i<3; ++i)
+	{
+		
+		mat2[i] = new int[3];
+		for(int j=0; j<3; ++j)
+	    {
+	    	contador++;
+	    	
+	    	cout << "Digite o valor " << contador << ":";
+			cin >> mat2[i][j];
+	    }
+	}
+	for(int i=0; i<3; ++i)
+	{
+	    for(int j=0; j<3; ++j)
+	    {
+            cout << mat2[i][j] << " | ";
+		}
+		cout << endl; 
+	}	
+	
+	comparaMatrizes(mat1, mat2);
+	
+	delete[] mat1;
+	delete[] mat2;
+	
+	return 0;
+}
